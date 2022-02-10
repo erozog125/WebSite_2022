c03363a58ca57ab05173f885a7954300b0c8c7e3
Edwin Rozo Gómez <erozog125@gmail.com>
2022-02-10 08:32:28 -0500

    Add page 404

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ import {
 import { Home } from './components/Pages/Home/Home';
 import { AboutUs } from './components/Pages/AboutUs/AboutUs';
 import { Contact } from './components/Pages/Contact/Contac';
+import { NotFound } from './components/Pages/NotFound/NotFound';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/prueba' element={<h1>Hola</h1>} />
         <Route path='/' element={<Home />} />
         <Route path='/aboutUs' element={<AboutUs />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.css';
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h2>Page Not found :(</h2>
+      <iframe src="https://embed.lottiefiles.com/animation/58718"></iframe>
+    </div>
+  )
+}
